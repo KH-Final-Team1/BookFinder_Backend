a4ef0c3e92ce6d4ce90f63c19766d6c6e9e85cf7
JinHoooooou <jinho4744@naver.com>
2024-04-15 08:51:42 +0900

    first commit

---
+++ src/main/java/com/kh/bookfinder/BookFinderApplication.java
@@ -0,0 +1,13 @@
+package com.kh.bookfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookFinderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kh/bookfinder/BookFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.bookfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFinderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
