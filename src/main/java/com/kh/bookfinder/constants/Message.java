f45b3ea47bfcd862b77ed87bc2f94ac1849079e1
JinHoooooou <jinho4744@naver.com>
2024-05-03 12:22:50 +0900

    JH-31: SignUp API 성공 테스트 및 로직 추가: Controller, Service, Repository 추가 SignUpDto 추가, SignUpDto에 대한 Validation Check 추가 비밀번호 암호화 추가

---
+++ src/main/java/com/kh/bookfinder/constants/Message.java
@@ -0,0 +1,16 @@
+package com.kh.bookfinder.constants;
+
+public interface Message {
+
+  String SIGNUP_SUCCESS = """
+      가입이 완료되었습니다.
+      환영합니다.
+      로그인 후 이용해 주세요 😊
+      """;
+  String BAD_REQUEST = "요청이 유효하지 않습니다. 다시 한번 확인해주세요.";
+  String INVALID_EMAIL = "유효하지 않은 이메일 형식입니다.";
+  String INVALID_PASSWORD = "영문, 숫자, 특수기호를 포함하여 8자 이상 20자 이하로 입력해주세요.";
+  String INVALID_PASSWORD_CONFIRM = "비밀번호와 비밀번호 확인이 일치하지 않습니다.";
+  String INVALID_NICKNAME = "영문, 유효한 한글, 숫자를 이용하여 3자 이상 10자 이하로 입력해주세요.";
+  String INVALID_PHONE = "유효하지 않은 휴대폰 번호 형식입니다.";
+}
--- src/main/java/com/kh/bookfinder/repository/UserRepository.java
+++ src/main/java/com/kh/bookfinder/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.kh.bookfinder.repository;
 
 import com.kh.bookfinder.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findByEmail(String email);
 }
