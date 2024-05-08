ef7c4cc227fee579b9073deb8247f2dc8c4bdb6a
min-dro <serominsockji9n9@gmail.com>
2024-05-08 18:01:21 +0900

    MS-46: 도서 상세 조회 API 작성: Controller, Service 추가, Message NOT_FOUND_ISBN 상수 추가

--- src/main/java/com/kh/bookfinder/controller/BookController.java
+++ src/main/java/com/kh/bookfinder/controller/BookController.java
@@ -1,13 +1,18 @@
 package com.kh.bookfinder.controller;
 
+import com.kh.bookfinder.constants.Message;
 import com.kh.bookfinder.dto.SearchDto;
 import com.kh.bookfinder.entity.Book;
 import com.kh.bookfinder.service.BookService;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +25,16 @@ public class BookController {
   public ResponseEntity<List<Book>> selectList(@Valid SearchDto requestParam) {
     List<Book> bookList = bookService.selectList(requestParam);
     return ResponseEntity.ok().body(bookList);
+  }
+
+  @GetMapping(value = "api/v1/books/{isbn}", produces = "application/json;charset=UTF-8")
+  public ResponseEntity<Object> selectBookRequestIsbn(@PathVariable(name = "isbn") Long isbn) throws JSONException {
+    if ((long) (Math.log10(isbn) + 2) == 13) {
+      JSONObject responseBody = new JSONObject();
+      responseBody.put("message", Message.INVALID_ISBN_DIGITS);
+      return ResponseEntity.badRequest().body(responseBody.toString());
+    }
+    Optional<Book> book = bookService.findBook(isbn);
+    return ResponseEntity.ok().body(book);
   }
 }
