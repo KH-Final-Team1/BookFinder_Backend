5115b00e97b66a5171d051eea0a5112dc20da82a
hankyungeun <az2282@naver.com>
2024-05-10 17:17:11 +0900

    KE-0: 리팩토링: book, bookTrade 예외처리 수정, DTO->Dto 명칭 수정

--- src/main/java/com/kh/bookfinder/dto/BookTradeDto.java
+++ src/main/java/com/kh/bookfinder/dto/BookTradeDto.java
@@ -7,10 +7,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.util.Date;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
-public class BookTradeDTO {
+public class BookTradeDto {
 
   @Min(value = 1000000000000L, message = Message.INVALID_ISBN_DIGITS)
   @Max(value = 9999999999999L, message = Message.INVALID_ISBN_DIGITS)
@@ -20,7 +19,6 @@ public class BookTradeDTO {
   @Min(value = 0, message = Message.INVALID_COST)
   @Max(value = 100000, message = Message.INVALID_COST)
   private Integer rentalCost;
-  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
   private Date limitedDate;
   private String content;
   private float latitude;
--- src/main/java/com/kh/bookfinder/service/BookService.java
+++ src/main/java/com/kh/bookfinder/service/BookService.java
@@ -1,11 +1,12 @@
 package com.kh.bookfinder.service;
 
+import com.kh.bookfinder.constants.Message;
 import com.kh.bookfinder.dto.SearchDto;
 import com.kh.bookfinder.entity.Book;
 import com.kh.bookfinder.repository.BookRepository;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +15,9 @@ public class BookService {
   @Autowired
   BookRepository bookRepository;
 
-  public Optional<Book> findBook(Long isbn) {
-    return bookRepository.findByIsbn(isbn);
+  public Book findBook(Long isbn) {
+    return bookRepository.findByIsbn(isbn)
+        .orElseThrow(() -> new ResourceNotFoundException(Message.UNSAVED_ISBN));
   }
 
   public List<Book> selectList(SearchDto requestParam) {
