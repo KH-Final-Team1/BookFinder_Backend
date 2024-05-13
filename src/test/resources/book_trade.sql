3fd73c8e39aa46adf721c9b9a3e5f5dee38c19e7
hankyungeun <az2282@naver.com>
2024-05-13 10:56:32 +0900

    KE-0: 리팩토링: TradeType의 RENT를 LEND로 변경

--- src/test/resources/book_trade.sql
+++ src/test/resources/book_trade.sql
@@ -5,7 +5,7 @@ values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 97911916004
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 120000, 1, '2024-05-11 17:49:21', 9791191905076, '2024-05-16 17:49:33', 2,
-        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'RENT', 'N');
+        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'LEND', 'N');
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 9791191600414, '2024-05-14 17:48:05', 3,
@@ -13,7 +13,7 @@ values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 97911916004
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 120000, 1, '2024-05-11 17:49:21', 9791191905076, '2024-05-16 17:49:33', 4,
-        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'RENT', 'N');
+        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'LEND', 'N');
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 9791191600414, '2024-05-14 17:48:05', 5,
@@ -21,7 +21,7 @@ values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 97911916004
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 120000, 1, '2024-05-11 17:49:21', 9791191905076, '2024-05-16 17:49:33', 6,
-        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'RENT', 'N');
+        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'LEND', 'N');
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 9791191600414, '2024-05-14 17:48:05', 7,
@@ -29,7 +29,7 @@ values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 97911916004
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 120000, 1, '2024-05-11 17:49:21', 9791191905076, '2024-05-16 17:49:33', 8,
-        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'RENT', 'N');
+        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'LEND', 'N');
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 9791191600414, '2024-05-14 17:48:05', 9,
@@ -37,4 +37,4 @@ values (37.49805400, 127.02872700, 100000, 1, '2024-05-11 17:47:48', 97911916004
 insert into book_trade (latitude, longitude, rental_cost, borough_id, create_date, isbn, limited_date, trade_id,
                         update_date, user_id, content, delete_yn, trade_type, trade_yn)
 values (37.49805400, 127.02872700, 120000, 1, '2024-05-11 17:49:21', 9791191905076, '2024-05-16 17:49:33', 10,
-        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'RENT', 'N');
+        '2024-05-11 17:49:39', 1, '책 빌려줘~', 'N', 'LEND', 'N');
