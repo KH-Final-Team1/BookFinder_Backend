b5d7ecddf1711ad7ecb460a498aa87ace1e34467
JinHoooooou <jinho4744@naver.com>
2024-05-26 19:38:49 +0900

    JH-108: 인증과 인가 테스트 분리

--- src/main/java/com/kh/bookfinder/global/constants/Message.java
+++ src/main/java/com/kh/bookfinder/global/constants/Message.java
@@ -26,8 +26,8 @@ public interface Message {
   String INVALID_PHONE = "유효하지 않은 휴대폰 번호 형식입니다.";
   String INVALID_AUTH_CODE = "유효하지 않은 인증 코드입니다.";
   String EXPIRED_AUTH_CODE = "인증 코드가 만료되었습니다.";
-  String INVALID_SIGNING_TOKEN = "유효하지 않은 토큰입니다.";
-  
+  String INVALID_SIGNING_TOKEN = "유효하지 않은 Signing 토큰입니다.";
+
   String FAIL_LOGIN = "이메일 또는 비밀번호가 올바르지 않습니다.";
   String INVALID_LOGIN_WITH_AUTHORIZATION = "헤더에 Authorization이 있어 로그인 요청을 할 수 없습니다.";
   String ALREADY_LOGIN = "이미 로그인 되어 있습니다.";
@@ -35,8 +35,8 @@ public interface Message {
   String NOT_FOUND_USER = "요청하신 사용자를 찾을 수 없습니다.";
 
   String INVALID_JWT_SIGNATURE = "잘못된 JWT 서명입니다.";
-  String INVALID_ACCESS_TOKEN = "유효하지 않은 JWT 토큰입니다.";
-  String EXPIRED_ACCESS_TOKEN = "만료된 JWT 토큰입니다.";
+  String INVALID_ACCESS_TOKEN = "유효하지 않은 토큰입니다.";
+  String EXPIRED_ACCESS_TOKEN = "만료된 토큰입니다.";
 
   String INVALID_ISBN_DIGITS = "ISBN 번호는 13자리의 숫자만 입력 가능합니다.";
   String UNSAVED_ISBN = "유효하지 않은 ISBN 번호 입니다.";
--- src/test/java/com/kh/bookfinder/auth/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/com/kh/bookfinder/auth/jwt/JwtAuthenticationFilterTest.java
@@ -10,9 +10,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.kh.bookfinder.auth.jwt.service.JwtService;
 import com.kh.bookfinder.global.constants.Message;
 import com.kh.bookfinder.user.entity.User;
-import com.kh.bookfinder.user.entity.UserRole;
 import com.kh.bookfinder.user.helper.MockUser;
 import com.kh.bookfinder.user.repository.UserRepository;
+import io.jsonwebtoken.JwtException;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,164 +43,110 @@ public class JwtAuthenticationFilterTest {
   private UserRepository userRepository;
 
   @Test
-  @DisplayName("Anonymous 권한 경로에 권한 없이 접근 하는 경우")
-  public void success_OnAnonymousAuthorization_WithNotAuthorization() throws Exception {
-    // When: anonymous 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/anonymous"));
+  @DisplayName("Header의 Authorization이 유효한 경우")
+  public void success_OnAuthenticatedAuthorization_WithValidAuthorization() throws Exception {
+    // Given: 유효한 Jwt 토큰이 주어진다.
+    String validJwtToken = "validToken";
+    // And: User가 주어진다.
+    User mockUser = MockUser.getMockUser();
+    // And: JwtService가 validJwtToken을 반환하도록 Mocking한다.
+    when(jwtService.extractAccessToken(any())).thenReturn(validJwtToken);
+    // And: JwtService가 true를 반환하도록 Mocking한다.
+    when(jwtService.validateToken(validJwtToken)).thenReturn(true);
+    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
+    when(jwtService.extractEmail(validJwtToken)).thenReturn(mockUser.getEmail());
+    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
+    when(userRepository.findByEmail(mockUser.getEmail())).thenReturn(Optional.of(mockUser));
+
+    // When: authenticated 권한 경로로 요청을 보낸다.
+    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate")
+        .header("Authorization", validJwtToken));
 
     // Then: Status는 Not Found이다.
     resultActions.andExpect(status().isNotFound());
   }
 
   @Test
-  @DisplayName("Anonymous 권한 경로에 권한을 가지고 접근하는 경우")
-  public void fail_OnAnonymousAuthorization_WithAuthorization() throws Exception {
-    // Given: ADMIN 권한을 가진 User가 주어진다.
-    User mockUser = MockUser.getMockUser();
-    mockUser.setRole(UserRole.ROLE_ADMIN);
-    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
-    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
-    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
-    when(userRepository.findByEmail(mockUser.getEmail()))
-        .thenReturn(Optional.of(mockUser));
-
-    // When: anonymous 권한 경로로 Token을 담아 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(
-        get("/test/v1/anonymous")
-            .header("Authorization", "mockToken"));
-
-    // Then: Status는 Forbidden이다.
-    resultActions.andExpect(status().isForbidden());
-  }
-
-  @Test
-  @DisplayName("Authenticated 권한 경로에 권한 없이 접근하는 경우")
-  public void fail_OnAuthenticatedAuthorization_WithNotAuthorization() throws Exception {
-    // Given: JwtService가 Exception을 발생하도록 Mocking한다.
-    when(jwtService.validateToken(any())).thenThrow(new IllegalArgumentException(Message.NOT_LOGIN));
+  @DisplayName("Jwt의 서명이 잘못된 경우")
+  public void fail_OnAuthenticatedAuthorization_WithInvalidJwtSignature() throws Exception {
+    // Given: 유효하지 않은 Jwt 토큰이 주어진다.
+    String invalidJwtToken = "invalidToken";
+    // And: JwtService가 validJwtToken을 반환하도록 Mocking한다.
+    when(jwtService.extractAccessToken(any())).thenReturn(invalidJwtToken);
+    // And: JwtService가 Exception을 발생하도록 Mocking한다.
+    when(jwtService.validateToken(invalidJwtToken)).thenThrow(new JwtException(Message.INVALID_JWT_SIGNATURE));
 
     // When: authenticated 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate"));
+    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate")
+        .header("Authorization", invalidJwtToken));
 
     // Then: Status는 Unauthorized이다.
     resultActions.andExpect(status().isUnauthorized());
     // And: Response Body로 message와 detail을 반환한다.
     resultActions.andExpect(jsonPath("$.message", is(Message.UNAUTHORIZED)));
-    resultActions.andExpect(jsonPath("$.detail", is(Message.NOT_LOGIN)));
+    resultActions.andExpect(jsonPath("$.detail", is(Message.INVALID_JWT_SIGNATURE)));
   }
 
   @Test
-  @DisplayName("Authenticated 권한 경로에 권한을 가지고 접근하는 경우")
-  public void success_OnAuthenticatedAuthorization_WithAuthorization() throws Exception {
-    // Given: USER 권한을 가진 User가 주어진다.
-    User mockUser = MockUser.getMockUser();
-    mockUser.setRole(UserRole.ROLE_USER);
-    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
-    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
-    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
-    when(userRepository.findByEmail(mockUser.getEmail()))
-        .thenReturn(Optional.of(mockUser));
+  @DisplayName("Jwt가 유효하지 않은 경우")
+  public void fail_OnAuthenticatedAuthorization_WithInvalidJwt() throws Exception {
+    // Given: 유효하지 않은 Jwt 토큰이 주어진다.
+    String invalidJwtToken = "invalidToken";
+    // And: JwtService가 validJwtToken을 반환하도록 Mocking한다.
+    when(jwtService.extractAccessToken(any())).thenReturn(invalidJwtToken);
+    // And: JwtService가 Exception을 발생하도록 Mocking한다.
+    when(jwtService.validateToken(invalidJwtToken)).thenThrow(new JwtException(Message.INVALID_ACCESS_TOKEN));
 
     // When: authenticated 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate"));
-
-    // Then: Status는 Not Found이다.
-    resultActions.andExpect(status().isNotFound());
-  }
-
-  @Test
-  @DisplayName("ADMIN 권한 경로에 ADMIN 권한을 가지고 접근하는 경우")
-  public void success_OnAdminAuthorization_WithAdminAuthorization() throws Exception {
-    // Given: ADMIN 권한을 가진 User가 주어진다.
-    User mockUser = MockUser.getMockUser();
-    mockUser.setRole(UserRole.ROLE_ADMIN);
-    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
-    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
-    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
-    when(userRepository.findByEmail(mockUser.getEmail()))
-        .thenReturn(Optional.of(mockUser));
-
-    // When: Admin 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/admin"));
-
-    // Then: Status는 Not Found이다.
-    resultActions.andExpect(status().isNotFound());
-  }
-
-  @Test
-  @DisplayName("ADMIN 권한 경로에 USER 권한을 가지고 접근하는 경우")
-  public void fail_OnAdminAuthorization_WithUserAuthorization() throws Exception {
-    // Given: USER 권한을 가진 User가 주어진다.
-    User mockUser = MockUser.getMockUser();
-    mockUser.setRole(UserRole.ROLE_USER);
-    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
-    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
-    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
-    when(userRepository.findByEmail(mockUser.getEmail()))
-        .thenReturn(Optional.of(mockUser));
-
-    // When: Admin 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/admin"));
-
-    // Then: Status는 Forbidden이다.
-    resultActions.andExpect(status().isForbidden());
-  }
-
-  @Test
-  @DisplayName("ADMIN 권한 경로에 권한 없이 접근하는 경우")
-  public void fail_OnAdminAuthorization_WithNotAuthorization() throws Exception {
-    // When: Admin 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/admin"));
+    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate")
+        .header("Authorization", invalidJwtToken));
 
     // Then: Status는 Unauthorized이다.
     resultActions.andExpect(status().isUnauthorized());
+    // And: Response Body로 message와 detail을 반환한다.
+    resultActions.andExpect(jsonPath("$.message", is(Message.UNAUTHORIZED)));
+    resultActions.andExpect(jsonPath("$.detail", is(Message.INVALID_ACCESS_TOKEN)));
   }
 
   @Test
-  @DisplayName("USER 권한 경로에 USER 권한을 가지고 접근하는 경우")
-  public void success_OnUserAuthorization_WithUserAuthorization() throws Exception {
-    // Given: USER 권한을 가진 User가 주어진다.
-    User mockUser = MockUser.getMockUser();
-    mockUser.setRole(UserRole.ROLE_USER);
-    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
-    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
-    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
-    when(userRepository.findByEmail(mockUser.getEmail()))
-        .thenReturn(Optional.of(mockUser));
+  @DisplayName("Jwt가 만료된 경우")
+  public void fail_OnAuthenticatedAuthorization_WithExpiredToken() throws Exception {
+    // Given: 유효하지 않은 Jwt 토큰이 주어진다.
+    String invalidJwtToken = "invalidToken";
+    // And: JwtService가 validJwtToken을 반환하도록 Mocking한다.
+    when(jwtService.extractAccessToken(any())).thenReturn(invalidJwtToken);
+    // And: JwtService가 Exception을 발생하도록 Mocking한다.
+    when(jwtService.validateToken(invalidJwtToken)).thenThrow(new JwtException(Message.EXPIRED_ACCESS_TOKEN));
 
-    // When: User 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/user"));
-
-    // Then: Status는 Not Found이다.
-    resultActions.andExpect(status().isNotFound());
-  }
-
-  @Test
-  @DisplayName("USER 권한 경로에 ADMIN 권한을 가지고 접근하는 경우")
-  public void fail_OnUserAuthorization_WithAdminAuthorization() throws Exception {
-    // Given: Admin 권한을 가진 User가 주어진다.
-    User mockUser = MockUser.getMockUser();
-    mockUser.setRole(UserRole.ROLE_ADMIN);
-    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
-    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
-    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
-    when(userRepository.findByEmail(mockUser.getEmail()))
-        .thenReturn(Optional.of(mockUser));
-
-    // When: User 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/user"));
-
-    // Then: Status는 Not Found이다.
-    resultActions.andExpect(status().isNotFound());
-  }
-
-  @Test
-  @DisplayName("User 권한 경로에 권한 없이 접근하는 경우")
-  public void fail_OnUserAuthorization_WithNotAuthorization() throws Exception {
-    // When: User 권한 경로로 요청을 보낸다.
-    ResultActions resultActions = mockMvc.perform(get("/test/v1/user"));
+    // When: authenticated 권한 경로로 요청을 보낸다.
+    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate")
+        .header("Authorization", invalidJwtToken));
 
     // Then: Status는 Unauthorized이다.
     resultActions.andExpect(status().isUnauthorized());
+    // And: Response Body로 message와 detail을 반환한다.
+    resultActions.andExpect(jsonPath("$.message", is(Message.UNAUTHORIZED)));
+    resultActions.andExpect(jsonPath("$.detail", is(Message.EXPIRED_ACCESS_TOKEN)));
+  }
+
+  @Test
+  @DisplayName("Jwt에서 추출한 email이 DB에 없는 경우")
+  public void fail_OnAuthenticatedAuthorization_WithExtractedEmailNotExistInDB() throws Exception {
+    // Given: 유효한 Jwt 토큰이 주어진다.
+    String validToken = "validToken";
+    // And: JwtService가 validToken을 반환하도록 Mocking한다.
+    when(jwtService.extractAccessToken(any())).thenReturn(validToken);
+    // And: JwtService가 true를 반환하도록 Mocking한다.
+    when(jwtService.validateToken(validToken)).thenReturn(true);
+
+    // When: authenticated 권한 경로로 요청을 보낸다.
+    ResultActions resultActions = mockMvc.perform(get("/test/v1/authenticate")
+        .header("Authorization", validToken));
+
+    // Then: Status는 Unauthorized이다.
+    resultActions.andExpect(status().isUnauthorized());
+    // And: Response Body로 message와 detail을 반환한다.
+    resultActions.andExpect(jsonPath("$.message", is(Message.UNAUTHORIZED)));
+    resultActions.andExpect(jsonPath("$.detail", is(Message.NOT_FOUND_USER)));
   }
 }
