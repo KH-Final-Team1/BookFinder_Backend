bfd0fbcdb0ba692df7bbdadcd718af989517a630
JinHoooooou <jinho4744@naver.com>
2024-05-26 18:43:09 +0900

    JH-108: Anonymous 권한에 대한 테스트 추가

---
+++ src/test/java/com/kh/bookfinder/auth/jwt/JwtAuthenticationFilterTest.java
@@ -0,0 +1,73 @@
+package com.kh.bookfinder.auth.jwt;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.kh.bookfinder.auth.jwt.service.JwtService;
+import com.kh.bookfinder.user.entity.User;
+import com.kh.bookfinder.user.entity.UserRole;
+import com.kh.bookfinder.user.helper.MockUser;
+import com.kh.bookfinder.user.repository.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+public class JwtAuthenticationFilterTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  @MockBean
+  private JwtService jwtService;
+  @MockBean
+  private UserRepository userRepository;
+
+  @Test
+  @DisplayName("Anonymous 권한 경로에 권한 없이 접근 하는 경우")
+  public void success_OnAnonymousAuthorization_WithNotAuthorization() throws Exception {
+    // When: anonymous 권한 경로로 요청을 보낸다.
+    ResultActions resultActions = mockMvc.perform(get("/test/v1/anonymous"));
+
+    // Then: Status는 Not Found이다.
+    resultActions.andExpect(status().isNotFound());
+  }
+
+  @Test
+  @DisplayName("Anonymous 권한 경로에 권한을 가지고 접근하는 경우")
+  public void fail_OnAnonymousAuthorization_WithAuthorization() throws Exception {
+    // Given: ADMIN 권한을 가진 User가 주어진다.
+    User mockUser = MockUser.getMockUser();
+    mockUser.setRole(UserRole.ROLE_ADMIN);
+    // And: JwtService가 mockUser의 email을 반환하도록 Mocking한다.
+    when(jwtService.extractEmail(any())).thenReturn(mockUser.getEmail());
+    // And: UserRepository가 mockUser를 반환하도록 Mocking한다.
+    when(userRepository.findByEmail(mockUser.getEmail()))
+        .thenReturn(Optional.of(mockUser));
+
+    // When: anonymous 권한 경로로 Token을 담아 요청을 보낸다.
+    ResultActions resultActions = mockMvc.perform(
+        get("/test/v1/anonymous")
+            .header("Authorization", "mockToken"));
+
+    // Then: Status는 Forbidden이다.
+    resultActions.andExpect(status().isForbidden());
+  }
+
+}
