d1b42dbd770a9a65949506020ef45830d948b21b
JinHoooooou <jinho4744@naver.com>
2024-05-28 10:47:49 +0900

    JH-0: 주소 정규식 '서울'도 포함

--- src/main/java/com/kh/bookfinder/global/constants/Regexp.java
+++ src/main/java/com/kh/bookfinder/global/constants/Regexp.java
@@ -7,5 +7,5 @@ public interface Regexp {
   String NICKNAME = "^[가-힣a-zA-Z0-9]{3,20}$";
   String PHONE = "^01[0-9][0-9]{3,4}[0-9]{4}$";
   String AUTH_CODE = "^[a-zA-Z0-9]{8}$";
-  String ADDRESS = "^서울시 [가-힣]+구 .+$";
+  String ADDRESS = "^서울(시)? [가-힣]+구 .+$";
 }
