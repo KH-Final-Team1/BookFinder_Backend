d410352a57675adbca150ec635b599107b25ec6e
JinHoooooou <jinho4744@naver.com>
2024-06-18 14:13:07 +0900

    JH-0: sql파일 수정

--- src/test/resources/borough.sql
+++ src/test/resources/borough.sql
@@ -1,50 +1,50 @@
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (1, '강남구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (2, '강동구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (3, '강북구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (4, '강서구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (5, '관악구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (6, '광진구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (7, '구로구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (8, '금천구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (9, '노원구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (10, '도봉구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (11, '동대문구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (12, '동작구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (13, '마포구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (14, '서대문구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (15, '서초구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (16, '성동구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (17, '성북구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (18, '송파구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (19, '양천구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (20, '영등포구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (21, '용산구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (22, '은평구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (23, '종로구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (24, '중구');
-insert into borough (borough_id, name)
+insert into BOROUGH (BOROUGH_ID, NAME)
 values (25, '중랑구');
